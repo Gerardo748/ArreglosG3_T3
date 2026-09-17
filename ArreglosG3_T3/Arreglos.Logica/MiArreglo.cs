acca6a561e2cf02a5527df3ecaafff7118b345fc
Gerardo748 <25030233@itesa.edu.mx>
2026-09-17 08:18:07 -0600

    version_02:metodod llenar arreglo

---
+++ ArreglosG3_T3/Arreglos.Logica/MiArreglo.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arreglos.Logica
+{
+    public class MiArreglo
+    {
+        //campos o atributos
+        private int _tope;
+        private int[] _arreglo;
+        //Constructor
+        public MiArreglo(int n) {
+            N = n;
+            _arreglo = new int[N];
+            _tope = 0;
+        }
+        //propiedades
+        public int N { get; }
+        public bool EstaLleno => _tope == N;
+        public bool EstaVacio => _tope == 0;
+
+        //Metodos
+        public void Llenar(int minimo, int maximo)
+        {
+            Random random = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                _arreglo[i] = random.Next(minimo, maximo);     
+            }
+            _tope = N;
+        }
+        //Metodo ordenar
+        public void Ordenar()
+        {
+            for (int i = 0; i < _tope - 1; i++)
+            {
+                for (int j = i+; j < _tope; j++)
+                {
+                    if(_arreglo[i] > _arreglo[j])
+                    {
+                        Cambiar(ref _arreglo [i], ref _arreglo[j]);
+                    }
+                }
+            } 
+        }
+        //Metodo cambiar
+        public void Cambiar(ref int a, ref int b)
+        {
+            int aux = a;
+            a = b;
+            b = aux;
+        }
+        //Metodo ToString
+
+        public override string ToString()
+        {
+            if (EstaVacio)
+            {
+                Console.WriteLine("El arreglo esta vacio");
+            }
+                int contador = 0;
+                string salida = string.Empty;
+            for(int i = 0; i < _tope; i++)
+            {
+                salida += $"{_arreglo[i]}\t";
+                contador++;
+                if (contador > 9)
+                {
+                    salida += "\n";
+                    contador = 0;
+                }
+
+                contador = 0;
+                //salida = salida "\n";
+                salida += "\n";
+                {
+            
+            }   
+            return salida;
+        }
+
+    }
+}
+}
--- ArreglosG3_T3/Arreglos.UI.Consola/Program.cs
+++ ArreglosG3_T3/Arreglos.UI.Consola/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Arreglos.Logica;
+
+Console.WriteLine("Hello, World!");
+
+MiArreglo oMyArreglo = new MiArreglo(10);
+oMyArreglo.Llenar(1,100);
+
+//oMyArreglo.ToString();
+Console.WriteLine(oMyArreglo);
+Console.WriteLine("\narreglo desordenado");
+Console.WriteLine(oMyArreglo);
+Console.ReadKey();
