8f35c9ca1a8b9892a88091d7a018d51112aebb0e
Gerardo748 <25030233@itesa.edu.mx>
2026-09-21 08:41:11 -0600

    version_04: Metodo Ordena ascendente y descendente

--- ArreglosG3_T3/Arreglos.Logica/MiArreglo.cs
+++ ArreglosG3_T3/Arreglos.Logica/MiArreglo.cs
@@ -33,21 +33,37 @@ namespace Arreglos.Logica
             }
             _tope = N;
         }
-        //metodo ordenar
+        //metodo ordenenar (Burbuja)
         public void Ordenar()
+        { 
+            Ordenar(true);
+        }
+        public void Ordenar(bool ascendente)
         {
+
             for (int i = 0; i < _tope - 1; i++)
             {
                 for (int j = i + 1; j < _tope; j++)
                 {
-                    if (_arreglo[i] > _arreglo[j])
+                    if (ascendente)
                     {
-                        Cambiar(ref _arreglo[i], ref _arreglo[j]);
+                        if (_arreglo[i] > _arreglo[j])
+                        {
+                            Cambiar(ref _arreglo[i], ref _arreglo[j]);
+                        }
                     }
-                }
+                    else
+                    {
+                        if (_arreglo[i] < _arreglo[j])
+                        {
+                            Cambiar(ref _arreglo[i], ref _arreglo[j]);
+                        }
+                    }
 
+                }
             }
         }
+
         //metodo Caambiar
         public void Cambiar(ref int a, ref int b)
         {
--- ArreglosG3_T3/Arreglos.UI.Consola/Program.cs
+++ ArreglosG3_T3/Arreglos.UI.Consola/Program.cs
@@ -4,9 +4,20 @@ Console.WriteLine("Arreglos");
 MiArreglo oMyarreglo = new(10);
 oMyarreglo.Llenar(1, 10);
 oMyarreglo.ToString();
+
 Console.WriteLine("\nArreglo desordenado");
 Console.WriteLine(oMyarreglo.ToString());
+
 Console.WriteLine("\nArreglo ordenado");
 oMyarreglo.Ordenar();
+Console.WriteLine(oMyarreglo.ToString());
+
+Console.WriteLine("\nArreglo ordenado ascendente");
+oMyarreglo.Ordenar();
+Console.WriteLine(oMyarreglo.ToString());
+
+
+Console.WriteLine("\nArreglo ordenado descendente");
+oMyarreglo.Ordenar(false);
 Console.WriteLine(oMyarreglo.ToString());
 Console.ReadKey();
