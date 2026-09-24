fb65cbe1eca82f296850941deeab27c631277c86
Gerardo748 <25030233@itesa.edu.mx>
2026-09-24 08:24:28 -0600

    version_05:Metodos agregar y eliminar

--- ArreglosG3_T3/Arreglos.Logica/MiArreglo.cs
+++ ArreglosG3_T3/Arreglos.Logica/MiArreglo.cs
@@ -71,6 +71,60 @@ namespace Arreglos.Logica
             a = b;
             b = auxiliar;
         }
+        //Metodoagregar
+        public void Agregar(int numero)
+        {
+            if (EstaLeno)
+            {
+                throw new Exception("El arreglo esta lleno");
+            }
+            _arreglo[_tope] = numero;
+            _tope++;
+
+        }
+        //Metodod insertar
+                public void Insertar(int numero, int posicion)
+        {
+            if (EstaLeno)
+            {
+                   throw new Exception("El arreglo esta lleno");
+            }
+            if (posicion < 0)
+            {
+                posicion = 0;
+            }
+            if (posicion > _tope)
+            {
+             posicion = _tope;
+            }
+            for(int i = _tope; i > posicion; i--)
+            {
+               _arreglo[i] = _arreglo[i - 1];
+            }
+            _arreglo[posicion] = numero;
+            _tope++;
+        }
+        //Metodo eliminar
+        public void Eliminar(int posicion)
+        {
+            if (EstaVacio)
+            {
+                throw new Exception("El arreglo esta vacio");
+            }
+            if (posicion < 0)
+            {
+                posicion = 0;
+            }
+            if (posicion > _tope)
+            {
+                posicion = _tope;
+            }
+            for (int i = posicion; i < _tope - 1; i++)
+            {
+                _arreglo[i] = _arreglo[i + 1];
+            }
+            _tope--;
+        }
         //metodo ToString
         public override string ToString()
         {
--- ArreglosG3_T3/Arreglos.UI.Consola/Program.cs
+++ ArreglosG3_T3/Arreglos.UI.Consola/Program.cs
@@ -1,23 +1,54 @@
 ﻿using Arreglos.Logica;
 
-Console.WriteLine("Arreglos");
-MiArreglo oMyarreglo = new(10);
-oMyarreglo.Llenar(1, 10);
-oMyarreglo.ToString();
+        Console.WriteLine("Arreglos\n");
+//--------------------------------------------------------------
+MiArreglo oMyArreglo = new(5);
+oMyArreglo.Agregar(3);
+oMyArreglo.Agregar(5);
+oMyArreglo.Agregar(7);
 
-Console.WriteLine("\nArreglo desordenado");
-Console.WriteLine(oMyarreglo.ToString());
+oMyArreglo.Insertar(2, 0);
 
-Console.WriteLine("\nArreglo ordenado");
-oMyarreglo.Ordenar();
-Console.WriteLine(oMyarreglo.ToString());
+Console.WriteLine(oMyArreglo);
 
-Console.WriteLine("\nArreglo ordenado ascendente");
-oMyarreglo.Ordenar();
-Console.WriteLine(oMyarreglo.ToString());
+Console.WriteLine("----------------------------------------------------------");
+oMyArreglo.Eliminar(1);
+Console.WriteLine(oMyArreglo);
+//try
+//{
+//    for(int i = 0; i < oMyArreglo.N; i++)
+//    {
+//       oMyArreglo.Agregar(i*6);
+//    }
+//    oMyArreglo.Agregar(6);
+//    Console.WriteLine(oMyArreglo);
+
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine(ex.Message);
+
+//}
+
+//Console.ReadKey();
+
+//MiArreglo oMyarreglo = new(10);
+//        oMyarreglo.Llenar(1, 10);
+//        oMyarreglo.ToString();
+
+//        Console.WriteLine("\nArreglo desordenado");
+//        Console.WriteLine(oMyarreglo.ToString());
+
+//        Console.WriteLine("\nArreglo ordenado");
+//        oMyarreglo.Ordenar();
+//        Console.WriteLine(oMyarreglo.ToString());
+
+//        Console.WriteLine("\nArreglo ordenado ascendente");
+//        oMyarreglo.Ordenar();
+//        Console.WriteLine(oMyarreglo.ToString());
 
 
-Console.WriteLine("\nArreglo ordenado descendente");
-oMyarreglo.Ordenar(false);
-Console.WriteLine(oMyarreglo.ToString());
+//        Console.WriteLine("\nArreglo ordenado descendente");
+//        oMyarreglo.Ordenar(false);
+//        Console.WriteLine(oMyarreglo.ToString());
 Console.ReadKey();
